1ff39dfaecb5ab7385e560aacdc3c94731c69123
Alexander Grischuk <alexgrischuk@gmail.com>
2017-01-18 22:05:14 +0100

    Use firebase config directly

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ import { Header } from './components/common';
 
 class App extends Component {
   componentWillMount() {
-    firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp({
+      apiKey: 'AIzaSyC-2FSF3zNYzK9j7CUeftBOUgOltcLQ3Hs',
+      authDomain: 'auth-ac5d3.firebaseapp.com',
+      databaseURL: 'https://auth-ac5d3.firebaseio.com',
+      storageBucket: 'auth-ac5d3.appspot.com',
+      messagingSenderId: '690096267389',
+    });
   }
 
   render() {
