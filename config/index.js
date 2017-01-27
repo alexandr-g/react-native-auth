3ed202010dab75e7d30922aec929047bccacd8dd
Alexander Grischuk <alexgrischuk@gmail.com>
2017-01-27 17:06:53 +0100

    Remove duplicate firebase config

--- config/index.js
+++
@@ -1,7 +0,0 @@
-export default firebaseConfig = {
-  apiKey: 'AIzaSyC-2FSF3zNYzK9j7CUeftBOUgOltcLQ3Hs',
-  authDomain: 'auth-ac5d3.firebaseapp.com',
-  databaseURL: 'https://auth-ac5d3.firebaseio.com',
-  storageBucket: 'auth-ac5d3.appspot.com',
-  messagingSenderId: '690096267389',
-};
